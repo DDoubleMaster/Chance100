83499585a6ac9b19ccb34d581c2f84a8adc31dbf
DDoubleMaster <130893087+DDoubleMaster@users.noreply.github.com>
2025-02-17 20:52:04 +0500

    The game itself

---
+++ Chance100.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include <string>
+#include <cctype>
+#include <random>
+using namespace std;
+
+int main()
+{
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> random(1, 100);
+   
+
+    cout << "Press any key or Q for quit: " << endl;
+    string input;
+    while (getline(cin, input) && input != "Q") {
+        // Game parameters
+        int attemptCount = 5;
+        int wastedAttempt = 0; 
+        int currentNum;
+
+        cout << "Let's Play" << endl;
+        cout << "You must write num from 1 to 100" << endl;
+        cout << "You have only " << attemptCount << " attempt" << endl;
+        cout << "Good Luck" << endl;
+        
+        currentNum = random(gen);
+
+        while(wastedAttempt < attemptCount){
+            cout << "Attempt number " << wastedAttempt + 1 << ":";
+            int selectNum; cin >> selectNum;
+            if (cin.fail()) {
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                continue;
+            }
+
+            if (selectNum < 1 || selectNum > 100) {
+                cout << "Please, write num from 1 to 100" << endl;
+                continue;
+            }
+
+            wastedAttempt++;
+            if (selectNum == currentNum)
+            {
+                cout << "You are win!" << endl;
+                cout << "Wasted attempt: " << wastedAttempt << endl;
+                break;
+            }
+        }
+        if (wastedAttempt >= attemptCount)
+            cout << "You are Lost" << endl;
+        input.clear();
+        cout << "Press any key or Q for quit: " << endl;
+        getline(cin, input);
+    }
+
+    cout << "Good Bye";
+}
